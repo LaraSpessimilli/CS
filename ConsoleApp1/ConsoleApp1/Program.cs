824b39f698dc79be78d6b142c2f3b33c0a176659
LaraSpessimilli <larasossai63@gmail.com>
2024-09-01 12:57:28 -0300

    commit 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite um numero para ver se e par pou impar");
+            int NyNum = Convert.ToInt32(Console.ReadLine());
+            int nynum = NyNum / 2;
+            if (nynum == 0)
+            {
+                Console.WriteLine("par");
+            }
+            else 
+            {
+            
+            Console.WriteLine("impar");
+            
+            }
+
+        }
+    }
+}
+
